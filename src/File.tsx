997c5383b0de467bb66bf143631cb794c26f5758
Fideles <fidelis.marcelo@gmail.com>
2022-10-19 10:26:25 -0300

    Last modifications

--- src/File.tsx
+++ src/File.tsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import "./File.css";
+
 class File extends Component {
   
   constructor(props){
@@ -7,19 +9,22 @@ class File extends Component {
   }
 
   showFile = async (e) => {
+    var preview = document.getElementById('show-text')
     e.preventDefault()
     const reader = new FileReader()
     reader.onload = async (e) => {
       const text = (e.target?.result)
       console.log(text)
+      preview.innerHTML = text
     };
     reader.readAsText(e.target.files[0])
   }
 
   render = () => {
     return (
-      <div> 
+      <div id="page-map"> 
         <input type="file" onChange={(e) => this.showFile(e)} /> 
+        <div id="show-text">Choose text File</div>
       </div>
     )
   }
